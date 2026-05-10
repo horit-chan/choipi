ce14a00971224f967d8b1abf5ed8ab3c64f347ce
horit-chan <horitaisgood@gmail.com>
2026-05-10 12:02:29 +0900

    feat: Headerに各種ボタンを追加

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,11 +1,24 @@
 'use client';
 
+import Link from 'next/link';
+
 export default function Header() {
   return (
     <header className="bg-orange-300">
       <div className="flex p-6">
-        <h1 className="text-2xl font-bold">choipi</h1>
+        <Link href="/" className="text-2xl font-bold">
+          choipi
+        </Link>
+
         <div className="ml-6">ちょい足しレシピ共有サイト</div>
+
+        <Link href="login" className="rounded-xl border-4 p-1">
+          ログイン
+        </Link>
+
+        <Link href="mypage" className="rounded-xl border-4 p-1">
+          マイページ
+        </Link>
       </div>
     </header>
   );
